272cc7d76c3bef7585a37cf74bd77ec75f60cc3b
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-04-04 15:36:26 +0700

    init commit

--- src/App.java
+++ src/App.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        LinkedList<Integer> stack = new LinkedList<>();
+        Stack e = new Stack();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int decimal;
         System.out.println("Enter decimal:");
         decimal = Integer.parseInt(br.readLine());
         while(decimal!=0){
             int rem = decimal%2;
-            stack.push(rem);
+            e.push(rem);
             decimal = decimal /2;
         }
-
-        while (!stack.isEmpty()){
-            System.out.print(stack.pop());
+        while (!e.isEmpty()){
+            System.out.print(e.pop());
         }
     }
 }
+
